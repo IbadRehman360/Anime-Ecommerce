d913a9b769098cdce2ec61207cad9b166a167653
Ibad ur Rehman <ibadhashim4@gmail.com>
2023-10-26 12:50:02 +0500

    made few new changes

--- components/Home/LatestDrop.jsx
+++ components/Home/LatestDrop.jsx
@@ -1,6 +1,9 @@
+"use client";
+
 import Link from "next/link";
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 import LatestDropDisplay from "./LatestDropDisplay";
+import { useState } from "react";
 const products = [
   {
     id: 1,
@@ -94,119 +97,126 @@ const products = [
   },
 ];
 function LatestDrop() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const handlePrevPage = () => {
+    if (currentSlide > 0) {
+      setCurrentSlide(currentSlide - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (currentSlide < 1) {
+      setCurrentSlide(currentSlide + 1);
+    } else {
+      setCurrentSlide(0);
+    }
+  };
+
   return (
-    <div className=" border-gray-200 ">
-      <h2 className="  font-bungee  text-3xl lg:text-4xl tracking-wider font-semibold leading-6 text-center mt-10 lg:mt-12 text-gray-700">
-        Latest Drops{" "}
-      </h2>
-      <div className="mt-8 bg-[#fdfdfd]">
-        <div className="my-8 mr-2 flex items-center justify-between">
-          <h2 className="ml-2 text-[1.4rem] font-semibold text-gray-700 md:text-[1.7rem] lg:text-[1.8rem] 3xl:text-[2rem]">
-            Trending Gift Cards
-          </h2>
-          <Link to={"/dashboard"}>
-            <div className="hidden items-center gap-2 sm:flex">
-              <span>Discover All</span>
-              <div className="rounded-full border-[1px] border-black bg-white p-1 shadow-sm">
-                <a onClick={handleNextPage} href="#" className="">
-                  <MdKeyboardArrowRight />
-                </a>
-              </div>
+    <div className="mt-8 mx-auto max-w-2xl px-2 py-2 sm:px-6 md:max-w-[110rem] w-full  bg-[#fdfdfd]">
+      <div className="mr-2 flex items-center justify-between">
+        <h2 className="  font-bungee  text-3xl lg:text-4xl tracking-wider font-semibold leading-6 text-center mt-10 lg:mt-12 text-gray-700">
+          Latest Drops{" "}
+        </h2>
+        <Link href={"/dashboard"}>
+          <div className="hidden items-center gap-2 sm:flex">
+            <span>Discover All</span>
+            <div className="rounded-full border-[1px] border-black bg-white p-1 shadow-sm">
+              <a onClick={handleNextPage} href="#" className="">
+                <MdKeyboardArrowRight />
+              </a>
             </div>
-          </Link>
-
-          <div className="sm:hidden">
-            {currentSlide < 1 && (
-              <div className="rounded-full border-[1px] border-black bg-white p-1 shadow-sm">
-                <a
-                  onClick={handleNextPage}
-                  href="#mbTrendingGiftCardsSlide2"
-                  className=""
-                >
-                  <MdKeyboardArrowRight />
-                </a>
-              </div>
-            )}
-            {currentSlide > 0 && (
-              <div className="rounded-full border-[1px] border-black bg-white p-1 shadow-sm">
-                <a
-                  onClick={handlePrevPage}
-                  href="#mbTrendingGiftCardsSlide1"
-                  className=""
-                >
-                  <MdKeyboardArrowLeft />
-                </a>
-              </div>
-            )}
           </div>
+        </Link>
+
+        <div className="sm:hidden">
+          {currentSlide < 1 && (
+            <div className="rounded-full border-[1px] border-black bg-white p-1 shadow-sm">
+              <a
+                onClick={handleNextPage}
+                href="#mbTrendingProductsSlide2"
+                className=""
+              >
+                <MdKeyboardArrowRight />
+              </a>
+            </div>
+          )}
+          {currentSlide > 0 && (
+            <div className="rounded-full border-[1px] border-black bg-white p-1 shadow-sm">
+              <a
+                onClick={handlePrevPage}
+                href="#mbTrendingProductsSlide1"
+                className=""
+              >
+                <MdKeyboardArrowLeft />
+              </a>
+            </div>
+          )}
         </div>
-
-        <div className="carousel-center   hidden w-full gap-x-4 rounded-lg sm:carousel">
-          <div
-            id="trendingGiftCardsSlide1"
-            className="carousel-item   w-full gap-2  xl:gap-4"
-          >
-            {visibleData.slice(0, 4).map((giftCard) => (
-              <LatestDrop key={giftCard.id} giftCard={giftCard} />
-            ))}
-          </div>
-          <div
-            id="trendingGiftCardsSlide2"
-            className="carousel-item w-full gap-4 sm:gap-5 lg:gap-8"
-          >
-            {visibleData.slice(0, 4).map((giftCard) => (
-              <LatestDrop key={giftCard.id} giftCard={giftCard} />
-            ))}
-          </div>
+      </div>
+      <div className="carousel-center hidden  sm:flex w-full gap-x-4 rounded-lg sm:carousel">
+        <div id="trendingProductsSlide1" className="carousel-item">
+          {products.slice(0, 4).map((Product) => (
+            <LatestDropDisplay key={Product.id} products={Product} />
+          ))}
         </div>
-
-        <div className="carousel relative w-full gap-x-4 rounded-lg sm:hidden">
-          <div
-            id="mbTrendingGiftCardsSlide1"
-            className="carousel-item w-full gap-1  sm:gap-5 md:gap-10"
-          >
-            {giftCardData.slice(0, 2).map((giftCard) => (
-              <LatestDrop key={giftCard.id} giftCard={giftCard} />
-            ))}
-          </div>
-          <div
-            id="mbTrendingGiftCardsSlide2"
-            className="carousel-item w-full gap-1 sm:gap-5 md:gap-10"
-          >
-            {giftCardData.slice(2, 4).map((giftCard) => (
-              <LatestDrop key={giftCard.id} giftCard={giftCard} />
-            ))}
-          </div>
+        <div
+          id="trendingProductsSlide"
+          className="carousel-item w-full gap-4 sm:gap-5 lg:gap-8"
+        >
+          {products.slice(0, 4).map((Product) => (
+            <LatestDropDisplay key={Product.id} products={Product} />
+          ))}
         </div>
+      </div>
 
-        <div className="mt-2 flex justify-center text-xs sm:mt-4 md:mt-6">
-          <p
-            className="text-[0.97rem] sm:text-[1rem] md:hidden"
-            style={{ letterSpacing: "0.1em" }}
-          >
-            {currentSlide < 1 ? "1/2" : "2/2"}
-          </p>
-          <div className="hidden justify-center md:flex">
-            <a
-              href="#trendingGiftCardsSlide1"
-              onClick={handlePrevPage}
-              className={`mr-1 block h-[4px] w-[28px] rounded-2xl ${
-                currentSlide === 0 ? "bg-[#f03827]" : "bg-[#888888]"
-              } `}
-              id="prevPage"
-            />
-            <a
-              href="#trendingGiftCardsSlide2"
-              onClick={handleNextPage}
-              className={`mr-1 block h-[4px] w-[28px] rounded-2xl ${
-                currentSlide === 1 ? "bg-[#f03827]" : "bg-[#888888]"
-              } `}
-            />
-          </div>
+      <div className="carousel relative w-full gap-x-4 rounded-lg sm:hidden">
+        <div
+          id="mbLatestDropDisplaysSlide1"
+          className="carousel-item w-full gap-1 sm:gap-5 md:gap-10"
+        >
+          {products.slice(0, 2).map((Product) => (
+            <LatestDropDisplay key={Product.id} products={Product} />
+          ))}
+        </div>
+        <div
+          id="mbLatestDropDisplaysSlide2"
+          className="carousel-item w-full gap-1 sm:gap-5 md:gap-10"
+        >
+          {products.slice(2, 4).map((Product) => (
+            <LatestDropDisplay key={Product.id} products={Product} />
+          ))}
+        </div>
+      </div>
+
+      <div className="mt-2 flex justify-center text-xs sm:mt-4 md:mt-6">
+        <p
+          className="text-[0.97rem] sm:text-[1rem] md:hidden"
+          style={{ letterSpacing: "0.1em" }}
+        >
+          {currentSlide < 1 ? "1/2" : "2/2"}
+        </p>
+        <div className="hidden justify-center md:flex">
+          <a
+            href="#trendingProductsSlide1"
+            onClick={handlePrevPage}
+            className={`mr-1 block h-[4px] w-[28px] rounded-2xl ${
+              currentSlide === 0 ? "bg-[#f03827]" : "bg-[#888888]"
+            } `}
+            id="prevPage"
+          />
+          <a
+            href="#trending
+            sSlide2"
+            onClick={handleNextPage}
+            className={`mr-1 block h-[4px] w-[28px] rounded-2xl ${
+              currentSlide === 1 ? "bg-[#f03827]" : "bg-[#888888]"
+            } `}
+          />
         </div>
       </div>
     </div>
   );
 }
-
 export default LatestDrop;
--- components/Home/LatestDropDisplay.jsx
+++ components/Home/LatestDropDisplay.jsx
@@ -1,82 +1,28 @@
-"use client";
-
-import { useState } from "react";
-
-export default function LatestDropDisplay() {
-  const [currentSlide, setCurrentSlide] = useState(0);
-
-  const handlePrevPage = () => {
-    if (currentSlide > 0) {
-      setCurrentSlide(currentSlide - 1);
-    }
-  };
-
-  const handleNextPage = () => {
-    if (currentSlide < 1) {
-      setCurrentSlide(currentSlide + 1);
-    } else {
-      setCurrentSlide(0);
-    }
-  };
-
-  const visibleProducts = products.slice(
-    currentSlide * 4,
-    (currentSlide + 1) * 4
-  );
+export default function LatestDropDisplay({ products }) {
   return (
     <div className="bg-white">
-      <div className="mx-auto max-w-2xl px-4 py-2 sm:px-6 lg:max-w-[110rem]">
-        <div className="mt-6 grid grid-cols-6 gap-x-8">
-          {visibleProducts.map((product) => (
-            <div key={product.id} className="group relative">
-              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                <img
-                  src={product.imageSrc}
-                  alt={product.imageAlt}
-                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                />
-              </div>
-              <div className="mt-4 flex justify-between">
-                <div>
-                  <h3 className="text-sm text-gray-700">
-                    <a href={product.href}>
-                      <span aria-hidden="true" className="absolute inset-0" />
-                      {product.name}
-                    </a>
-                  </h3>
-                  <p className="mt-1 text-sm text-gray-500">{product.color}</p>
-                </div>
-                <p className="text-sm font-medium text-gray-900">
-                  {product.price}
-                </p>
-              </div>
+      <div className="mx-auto w-full     ">
+        <div key={products?.id} className="group relative">
+          <div className=" h-80">
+            <img
+              src={products?.imageSrc}
+              alt={products?.imageAlt}
+              className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+            />
+          </div>
+          <div className="mt-4 flex justify-between">
+            <div>
+              <h3 className="text-sm text-gray-700">
+                <a href={products?.href}>
+                  <span aria-hidden="true" className="absolute inset-0" />
+                  {products?.name}
+                </a>
+              </h3>
+              <p className="mt-1 text-sm text-gray-500">{products?.color}</p>
             </div>
-          ))}
-        </div>
-
-        <div className="mt-2 flex justify-center text-xs sm:mt-4 md:mt-6">
-          <p
-            className="text-[0.97rem] sm:text-[1rem] md:hidden"
-            style={{ letterSpacing: "0.1em" }}
-          >
-            {currentSlide < 1 ? "1/2" : "2/2"}
-          </p>
-          <div className="hidden justify-center md:flex">
-            <a
-              href="#trendingGiftCardsSlide1"
-              onClick={handlePrevPage}
-              className={`mr-1 block h-[4px] w-[28px] rounded-2xl ${
-                currentSlide === 0 ? "bg-[#f03827]" : "bg-[#888888]"
-              } `}
-              id="prevPage"
-            />
-            <a
-              href="#trendingGiftCardsSlide2"
-              onClick={handleNextPage}
-              className={`mr-1 block h-[4px] w-[28px] rounded-2xl ${
-                currentSlide === 1 ? "bg-[#f03827]" : "bg-[#888888]"
-              } `}
-            />
+            <p className="text-sm font-medium text-gray-900">
+              {products?.price}
+            </p>
           </div>
         </div>
       </div>
