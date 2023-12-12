7a6b42042d92524e087d2d7d6d92f40c111f5a3b
Ibad ur Rehman <ibadhashim4@gmail.com>
2023-12-13 02:05:37 +0500

    made some more changes (2) on mobile design

--- components/Home/LatestDrop.jsx
+++ components/Home/LatestDrop.jsx
@@ -2,7 +2,7 @@ import LatestDropRender from "./LatestDropRender";
 
 async function LatestDrop({ products }) {
   return (
-    <div className="pt-8 pb-6 sm:pb-8 lg:pb-10 mx-auto md:px-6 px-2 py-2 w-full">
+    <div className="pt-7 sm:pt-8 pb-6 sm:pb-8 lg:pb-10 mx-auto md:px-6 px-2 py-2 w-full">
       <div className="text-center pb-0.5">
         <h2 className="  font-montserrat text-[1.7rem]        overflow-hidden     leading-9  font-extrabold sm:text-[2.2rem] lg:text-[2.6rem] uppercase mb-1 text-gray-900  tracking-wide sm:tracking-wider text-center lg:mt-6">
           <span className="sm:block hidden">
