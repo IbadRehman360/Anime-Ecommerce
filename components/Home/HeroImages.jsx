bc010c694b511ac60a96bdb66d81e59f263f0cbc
IbadRehman360 <ibadhashim4@example.com>
2024-01-01 06:51:22 +0500

    delete some images

--- components/Home/HeroImages.jsx
+++ components/Home/HeroImages.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 /* eslint-disable @next/next/no-img-element */
 export default function HeroImages({ index, imageUrl }) {
-  const images = ["/assets/mb1.png", "/assets/mb2.png", "/assets/mb3.png"];
+  const images = ["/assets/mb1.png", "/assets/mb2.png"];
   const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
@@ -15,7 +15,7 @@ export default function HeroImages({ index, imageUrl }) {
   return (
     <div className="relative">
       <img
-        className="hidden sm:flex border-x-4    border-y-2  border-black border-opacity-90      3xl:max-h-[40rem] max-h-[32rem] w-full mx-auto filter   brightness-100    contrast-100"
+        className="hidden sm:flex border-x-4     border-y-2  border-black border-opacity-90      3xl:max-h-[40rem] max-h-[32rem] w-full mx-auto filter   brightness-100    contrast-100"
         src={`/assets/effect.png`}
         alt={`bg ${index}`}
       />
@@ -23,7 +23,7 @@ export default function HeroImages({ index, imageUrl }) {
       <img
         src={images[currentIndex]}
         alt={`bg ${currentIndex + 1}`}
-        className="sm:hidden border border-black w-full mx-auto my-auto"
+        className="sm:hidden border min-h-[18rem]   max-h-[20rem] border-black w-full mx-auto my-auto"
       />
     </div>
   );
