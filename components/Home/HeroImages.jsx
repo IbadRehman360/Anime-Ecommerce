944e633119f20c9cf609784e6fb61e3dce41bd75
IbadRehman360 <ibadhashim4@example.com>
2024-01-21 16:48:00 +0500

    new changes

--- components/Home/HeroImages.jsx
+++ components/Home/HeroImages.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 /* eslint-disable @next/next/no-img-element */
 export default function HeroImages({ index, imageUrl }) {
-  const images = ["/assets/var.svg"];
+  const images = ["/assets/var.svg", "/assets/banner.jpg"];
   const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
@@ -16,14 +16,14 @@ export default function HeroImages({ index, imageUrl }) {
     <div className="relative">
       <img
         className="hidden sm:flex border-x-4     border-y-2  border-black border-opacity-90 3xl:max-h-[44rem]     max-h-[36rem] w-full mx-auto filter   brightness-100    contrast-100"
-        src={`/assets/effect.png`}
+        src={`/assets/pcvar.svg`}
         alt={`bg ${index}`}
       />
       <div className="absolute inset-0 bg-black  opacity-5 hover:opacity-0"></div>
       <img
-        src={images[currentIndex]}
+        src={`/assets/pcvar.svg`}
         alt={`bg ${currentIndex + 1}`}
-        className="sm:hidden border w-full   border-black    mx-auto my-auto"
+        className="sm:hidden border w-full h-80    object-fill   border-black    mx-auto my-auto"
       />
     </div>
   );
