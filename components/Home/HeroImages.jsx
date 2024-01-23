e3f3df2c21cc10a68876a5bda74af26f571e1bd6
IbadRehman360 <ibadhashim4@example.com>
2024-01-23 05:40:01 +0500

    updated the mb banner:

--- components/Home/HeroImages.jsx
+++ components/Home/HeroImages.jsx
@@ -2,28 +2,18 @@ import { useEffect, useState } from "react";
 
 /* eslint-disable @next/next/no-img-element */
 export default function HeroImages({ index, imageUrl }) {
-  const images = ["/assets/var.svg", "/assets/banner.jpg"];
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 3000);
-
-    return () => clearInterval(interval);
-  }, [currentIndex, images.length]);
   return (
     <div className="relative">
       <img
         className="hidden sm:flex border-x-4     border-y-4  border-black border-opacity-90 3xl:max-h-[44rem]     max-h-[36rem] w-full mx-auto filter   brightness-100    contrast-100"
         src={`/assets/WEB.svg`}
-        alt={`bg ${index}`}
+        alt={`pc banner`}
       />
 
       <img
-        src={images[currentIndex]}
-        alt={`bg ${currentIndex + 1}`}
-        className="sm:hidden border w-full h-80  object-cover   border-black    mx-auto my-auto"
+        src={"/assets/gr.svg"}
+        alt="mobile banner"
+        className="sm:hidden border-2 w-full    border-black    mx-auto my-auto"
       />
       <div className="absolute inset-0 bg-black  opacity-5 hover:opacity-0"></div>
     </div>
