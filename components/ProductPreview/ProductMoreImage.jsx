cbafc090ca0006cb401d306ebb5c320d8336cfa4
IbadRehman360 <ibadhashim4@gmail.com>
2024-09-20 19:58:09 +0500

    fix some issues

--- components/ProductPreview/ProductMoreImage.jsx
+++ components/ProductPreview/ProductMoreImage.jsx
@@ -8,23 +8,23 @@ export default function ProductMoreImage({
 }) {
   return (
     <div
-      className={`    ${
-        !selectedSize || !selectedColor
-          ? "    space-y-2 "
-          : " lg:flex lg:space-x-2     "
-      }`}
-    >
-      {product.images.map((src, index) => (
-        <Image
-          key={index}
-          width={70}
-          src={src}
-          height={70}
-          alt={`Small Image ${index}`}
-          onClick={() => handleImageClick(src)}
-          className={`  lg:h-28 lg:w-28  w-12  p-0.5 sm:w-16  border-2 rounded-md shadow-sm  border-gray-200 border-opacity-60   cursor-pointer  `}
-        />
-      ))}
-    </div>
+    className={`${
+      !selectedSize || !selectedColor
+        ? "space-y-2"
+        : "lg:flex lg:space-x-2"
+    }`}
+  >
+    {product.images.map((src, index) => (
+      <Image
+        key={index}
+        width={70}
+        height={70}
+        src={src} // Ensure the src is a valid Cloudinary URL
+        alt={`Small Image ${index}`}
+        onClick={() => handleImageClick(src)}
+        className={`lg:h-28 lg:w-28 w-12 p-0.5 sm:w-16 border-2 rounded-md shadow-sm border-gray-200 border-opacity-60 cursor-pointer`}
+      />
+    ))}
+  </div>
   );
 }
